82f531f2ed80563ca52651be1df293b2147bc6c5
liuqinghui <liuqinghui1991@163.com>
2020-10-20 19:39:33 +0800

    update

--- src/main/java/com/hui/logdemo/controller/LogController.java
+++ src/main/java/com/hui/logdemo/controller/LogController.java
@@ -1,14 +1,22 @@
 package com.hui.logdemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LogController {
-    private static final Logger logger = LoggerFactory.getLogger(LogController.class);
 
+    @RequestMapping("/log")
     public String test(){
-        return "";
+        log.trace("这是 trace 类型日志... {}","trace");
+        log.debug("这是 debug 类型日志... {}","debug");
+        log.info("这是 info 类型日志... {}","info");
+        log.warn("这是 warn 类型日志... {}","warn");
+        log.error("这是 error 类型日志... {}","error");
+        return "日志测试使用";
     }
 }
