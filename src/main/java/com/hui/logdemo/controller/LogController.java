d9fbac6b1f7ad8863c67620c13dfe442823dfa7d
liuqinghui <liuqinghui1991@163.com>
2020-10-24 18:35:51 +0800

    update

--- src/main/java/com/hui/logdemo/controller/LogController.java
+++ src/main/java/com/hui/logdemo/controller/LogController.java
@@ -6,17 +6,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @RestController
 @Slf4j
 public class LogController {
-
     @RequestMapping("/log")
     public String test(){
-        log.trace("这是 trace 类型日志... {}","trace");
-        log.debug("这是 debug 类型日志... {}","debug");
-        log.info("这是 info 类型日志... {}","info");
-        log.warn("这是 warn 类型日志... {}","warn");
-        log.error("这是 error 类型日志... {}","error");
+        String text = "日志内容~~~";
+        // 重复40遍字符串
+        String join = String.join("", Collections.nCopies(40, text));
+        log.trace("这是 trace 类型日志... {}",join);
+        log.debug("这是 debug 类型日志... {}",join);
+        log.info("这是 info 类型日志... {}",join);
+        log.warn("这是 warn 类型日志... {}",join);
+        log.error("这是 error 类型日志... {}",join);
         return "日志测试使用";
     }
 }
--- src/test/java/com/hui/logdemo/LogDemoApplicationTests.java
+++ src/test/java/com/hui/logdemo/LogDemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.hui.logdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
+@Slf4j
 class LogDemoApplicationTests {
-
     @Test
     void contextLoads() {
+        log.trace("这是 trace 类型日志... {}","trace");
+        log.debug("这是 debug 类型日志... {}","debug");
+        log.info("这是 info 类型日志... {}","info");
+        log.warn("这是 warn 类型日志... {}","warn");
+        log.error("这是 error 类型日志... {}","error");
     }
 
 }
